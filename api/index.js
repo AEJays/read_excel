9a97bca40d353ab0761ce68985189520d769a922
AEdgejay <471021444@qq.com>
2021-12-13 19:42:56 +0800

    multiple child table reutrns are supported

--- api/index.js
+++ api/index.js
@@ -16,18 +16,24 @@ let api = {
     async excel(ctx){
         const file = ctx.request.files.excel
         if(file){
-            let content = xlsx.parse(fs.readFileSync(file.path))[0].data
-            let title = content.splice(0,1)[0]
-            let list = []
-            content.map((item,i)=>{
-                let itemObject = {}
-                for(var a=0;a<title.length;a++){
-                    itemObject[title[a]] = item[a]
-                }
-                list.push(itemObject)
+            let Obj = {}
+            let xlsxContent = xlsx.parse(fs.readFileSync(file.path))
+            xlsxContent.map((Xlsx,x)=>{
+                let name = Xlsx.name
+                let content = Xlsx.data
+                let list = []
+                let title = content.splice(0,1)[0]
+                content.map((item,i)=>{
+                    let itemObject = {}
+                    for(var a=0;a<title.length;a++){
+                        itemObject[title[a]] = item[a]
+                    }
+                    list.push(itemObject)
+                })
+                Obj[name] = list
             })
             fs.rmSync(file.path)
-            ctx.body = {"code":200,"msg":lang.convertSuccess,"data":list}
+            ctx.body = {"code":200,"msg":lang.convertSuccess,"data":Obj}
             ctx.status = 200
         }else{
             ctx.body = {"code":500,"msg":lang.convertError}
--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 let config = {
     // en:English cn:中文 
     // Do not set it to English.(不设置为英文)
-    lang:"",
+    lang:"cn",
     port:8000,
     fileSize:200 // mean is 200MB（意思是200MB）
 }
